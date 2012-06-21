24631ca75d6041b15763b1a9ff6c59e6c65b7d71
Dustin Sallings <dustin@spy.net>
2012-06-21 00:28:32 -0700

    A tool to cat a serial device to stdout.

---
+++ serialcat/serialcat.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"os"
+
+	"github.com/dustin/rs232.go"
+)
+
+func parseMode(s string) (rv rs232.SerConf) {
+	switch s {
+	case "8N1":
+		rv = rs232.S_8N1
+	case "7E1":
+		rv = rs232.S_7E1
+	case "7O1":
+		rv = rs232.S_7O1
+	default:
+		log.Fatalf("Invalid mode: %v", s)
+	}
+	return
+}
+
+func main() {
+	baudRate := flag.Int("baud", 57600, "Baud rate")
+	mode := flag.String("mode", "8N1", "8N1 | 7E1 | 7O1")
+	flag.Parse()
+
+	port, err := rs232.OpenPort(flag.Arg(0), *baudRate,
+		parseMode(*mode))
+	if err != nil {
+		log.Fatalf("Error opening port: %s", err)
+	}
+
+	io.Copy(os.Stdout, &port)
+}
